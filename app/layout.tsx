518475bb05be155735ba81dde8cef1af524a9619
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-28 19:53:38 +0530

    Project Setup and Navbar Added

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Price Wise",
+  description: "track product price",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="max-w-10xl mx-auto">
+          <Navbar/>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
