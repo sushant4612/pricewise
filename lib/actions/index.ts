1db759539e522d1836a18f7d65a47015e12744a3
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-31 14:25:28 +0530

    ProductCard Component Added

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../util";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -15,8 +18,58 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
         let product = scrapedProduct;
 
-        // const existingProduct = await 
+        const existingProduct = await Product.findOne({url: scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory: any = [
+                ...existingProduct.priceHistory,
+                { price: scrapedProduct.currentPrice}
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory)
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate({
+            url: scrapedProduct.url
+        },
+        product,
+        { upsert: true, new: true}
+        )
+
+        revalidatePath(`/products/${newProduct._id}`)
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
+    }
+}
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({ _id: productId})
+
+        if(!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getAllProducts() {
+    try {
+        connectToDB();
+
+        const products = await Product.find();
+
+        return products;
+    } catch (error) {
+        console.log(error);
     }
 }
