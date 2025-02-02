0d487bf6491aafaf6f50c1308070e92f03dd5220
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-02-02 19:13:28 +0530

    Buy Now Button Added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -130,7 +130,19 @@ const ProductsDetails = async ({params: {id}}: Props) => {
             {product?.description?.split('\n')}
           </div>
         </div>
+
+        <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
+          <Image
+            src="/assets/icons/bag.svg"
+            alt="check"
+            width={22}
+            height={22}
+          />
+
+          <Link href="/" className="text-base text-white">Buy Now</Link>
+        </button>
       </div>
+      
     </div>
   )
 }
